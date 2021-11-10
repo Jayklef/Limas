39f2e0237a78c391a2597e1c716188f6c17fa23d
Jayklef <arhawhojerry@gmail.com>
2021-11-10 21:45:18 +0100

    Added Logger to the GenreController

--- src/main/java/com/jayklef/limas/controller/GenreController.java
+++ src/main/java/com/jayklef/limas/controller/GenreController.java
@@ -3,6 +3,8 @@ package com.jayklef.limas.controller;
 import com.jayklef.limas.exception.GenreNotFoundException;
 import com.jayklef.limas.model.Genre;
 import com.jayklef.limas.service.GenreService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,6 +15,8 @@ public class GenreController {
 
     private GenreService genreService;
 
+    private Logger logger = LoggerFactory.getLogger(GenreController.class);
+
     @Autowired
     public GenreController(GenreService genreService) {
         this.genreService = genreService;
@@ -20,11 +24,13 @@ public class GenreController {
 
     @PostMapping("/genres")
     public Genre saveGenre(Genre genre){
+        logger.info("Inside saveGenre in GenreController");
         return genreService.saveGenre(genre);
     }
 
     @GetMapping("/genres")
     public List<Genre> getAllGenre(){
+        logger.info("Inside getAllGenre in GenreController");
         return genreService.getAllGenre();
     }
 
